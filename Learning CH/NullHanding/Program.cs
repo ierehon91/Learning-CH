4667067b037707befc67344cd6c0dcf3e2fcf2a2
Andrey Plekhanov <ierehon91@gmail.com>
2022-01-15 09:54:27 +0300

    Добавлен проект NullHanding. (стр. 100)

---
+++ Learning CH/NullHanding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NullHanding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Addres addres = new Addres();
+            addres.Building = "21";
+            addres.Street = "Krupskoi";
+            addres.City = "Semiluki";
+            addres.Country = null;
+            addres.PrintAddres();
+        }
+    }
+
+    class Addres
+    {
+        public string? Building;
+        public string Street = string.Empty;
+        public string City = string.Empty;
+        public string? Country;
+
+        public void PrintAddres()
+        {
+            string print;
+            if (Country == null && Building == null) print = $"{City} city, {Street} street";
+            else if (Country == null) print = $"{City} city, {Street} street, {Building}";
+            else if (Building == null) print = $"{Country}, {City} city, {Street} street";
+            else print = $"{Country}, {City} city, {Street} street, {Building}";
+            System.Console.WriteLine(print);
+        }
+    }
+}
