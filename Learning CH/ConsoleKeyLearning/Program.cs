72d96c2dfe62ff11e9c45ae63199854984cefbdb
Andrey Plekhanov <ierehon91@gmail.com>
2022-01-15 10:09:43 +0300

    Добавлен проект ConsoleKeyLearning

---
+++ Learning CH/ConsoleKeyLearning/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleKeyLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                System.Console.Write("Нажмите кнопку на клавиатуре для получения данных: ");
+                key = GetConsumerKey();
+                Console.WriteLine($"Key: {key.Key}\nChar: {key.KeyChar}\nModifiers: {key.Modifiers}\n\n");
+            }
+            while (key.Key != System.ConsoleKey.Escape);
+
+            System.Console.WriteLine("End");
+        }
+
+        static ConsoleKeyInfo GetConsumerKey()
+        {
+            ConsoleKeyInfo consumerKey = Console.ReadKey();
+            System.Console.WriteLine();
+            return consumerKey;
+        }
+    }
+}
