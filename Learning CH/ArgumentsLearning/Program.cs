a5ee59ef24f1a5eec26c50fdba77465ca0cc7a60
Andrey Plekhanov <ierehon91@gmail.com>
2022-01-15 10:15:25 +0300

    Добавлен проект ArgumentsLearning

---
+++ Learning CH/ArgumentsLearning/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace ArgumentsLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 2) ChangeConsoleColor(args[0], args[1]);
+            WriteLine($"Count arguments: {args.Length}");
+            foreach (string arg in args)
+            {
+                WriteLine(arg);
+            }
+        }
+
+        static void ChangeConsoleColor(string foregroundColor, string backgroundColor)
+        {
+            ForegroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: foregroundColor,
+                ignoreCase: true
+            );
+
+            BackgroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: backgroundColor,
+                ignoreCase: true
+            );
+            System.Console.WriteLine();
+        }
+    }
+}
