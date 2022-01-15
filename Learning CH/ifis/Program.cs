60d2bb8a3ec3753491dffaa9dd83b80f1aa77425
Andrey Plekhanov <ierehon91@gmail.com>
2022-01-15 10:27:01 +0300

    Добавлен проект ifis

---
+++ Learning CH/ifis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ifis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object money = 50;
+
+            if (money is decimal i)
+            {
+                System.Console.WriteLine($"You have {i}$");
+            }
+            else
+            {
+                System.Console.WriteLine("No decimal type!!!");
+            }
+        }
+    }
+}
