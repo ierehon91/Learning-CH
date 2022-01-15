8078a8cb97fa6c16924d217f3ad5521e1da64457
Andrey Plekhanov <ierehon91@gmail.com>
2022-01-15 10:37:03 +0300

    Добавлен проект Password

---
+++ Learning CH/Password/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = string.Empty;
+            int accessCounter = 10;
+            bool isAccsess = false;
+
+            do
+            {
+                Console.Write("Введите пароль: ");
+                password = System.Console.ReadLine();
+                if (password == "qwerty")
+                {
+                    isAccsess = true;
+                    break;
+                }
+                accessCounter--;
+                System.Console.WriteLine($"Пароль неверный, попыток осталось: {accessCounter}.");
+            }
+            while (accessCounter > 0);
+
+            string message = isAccsess ? "Доступ открыт!" : "Доступ закрыт!";
+            Console.WriteLine(message);
+        }
+    }
+}
