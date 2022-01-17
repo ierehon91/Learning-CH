70be83bdc9ffae0414a4690ddec8f66e9d1decd3
Andrey Plekhanov <ierehon91@gmail.com>
2022-01-17 10:42:49 +0300

    Добавлен пример с оператором ?? для типа строка (сам проект)

---
+++ Learning CH/NullLearning/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NullLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringExample(10);
+        }
+
+        /// <summary>
+        /// Случайно приваиваем значение переменной message: либо null, либо строку.
+        /// Если null, то при помощи оператора ?? выведется значение, которые мы укажем.
+        /// </summary>
+        static void StringExample(int countPrint)
+        {
+            string message = null;
+            Random random = new Random();
+            for (int i = 0; i < countPrint; i++)
+            {
+                message = (random.Next(0, 2) == 0) ? null : "Not Null";
+                Console.WriteLine(message ?? "Значение message = null");
+            }
+        }
+    }
+}
