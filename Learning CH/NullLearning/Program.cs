dafcbf29895ecd31eff030fdd925b352e4c5d78e
Andrey Plekhanov <ierehon91@gmail.com>
2022-01-17 14:53:38 +0300

    Добавлен пример работы с оператором ??=

--- Learning CH/NullLearning/Program.cs
+++ Learning CH/NullLearning/Program.cs
@@ -7,6 +7,7 @@ namespace NullLearning
         static void Main(string[] args)
         {
             StringExample(10);
+            Console.WriteLine(GetLengthArray(GetArray(15)));
         }
 
         /// <summary>
@@ -23,5 +24,28 @@ namespace NullLearning
                 Console.WriteLine(message ?? "Значение message = null");
             }
         }
+
+        static int[] GetArray()
+        {     
+            return null;
+        }
+
+        static int[] GetArray(int length)
+        {
+            return new int[length];
+        }
+
+        /// <summary>
+        /// Метод возврата числа элементов в массиве целых чисел
+        /// При передачи массива со значением null вернёт 0
+        /// /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        static int GetLengthArray(int[] array)
+        {
+            array ??= new int[0];
+            return array.Length;
+        }
+
     }
 }
