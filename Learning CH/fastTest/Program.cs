a14a6c5da42baa1f91d629f9bb32939f48487a31
Andrey Plekhanov <ierehon91@gmail.com>
2022-01-19 22:39:30 +0300

    Проверка работы со строкой как с сылочным типом

--- Learning CH/fastTest/Program.cs
+++ Learning CH/fastTest/Program.cs
@@ -6,7 +6,18 @@ namespace fastTest
     {
         static void Main(string[] args)
         {
-            
+            string s = "hello";
+            ref string s2 = ref s;
+            Console.WriteLine(s2);
+            s = "world";
+            Console.WriteLine(s2);
+
+            int[] a = { 0 };
+            int[] a2 = a;
+            Console.WriteLine(a2[0]);
+            a[0] = 1488;
+            Console.WriteLine(a2[0]);
+
         }
     }
 }
