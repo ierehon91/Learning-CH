a1ca04d44b54438516015e53aaf41f1b51ba888b
Andrey Plekhanov <ierehon91@gmail.com>
2022-02-12 09:11:00 +0300

    Добавлен проект изучения модификаторов метода. Добавлены методы с модификтором ref и out

---
+++ Learning CH/ModMethodsRefOutInParams/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ModMethodsRefOutInParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            RefMod(ref a);
+
+            OutMod(out int b);
+            Console.WriteLine($"b : {b}");
+            
+        }
+
+        static void RefMod(ref int a)
+        {
+            a++;
+            Console.WriteLine($"a : {a}");
+        }
+
+        static void OutMod(out int b)
+        {
+            b = 10;
+            b--;
+        }
+    }
+}
