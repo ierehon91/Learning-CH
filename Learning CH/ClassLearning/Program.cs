5455e09009d72b489e7049c8be0e59c7c85b44e2
Andrey Plekhanov <ierehon91@gmail.com>
2022-02-16 09:50:26 +0300

    Создан проект изучения ООП. В проекте создан класс Студент

---
+++ Learning CH/ClassLearning/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstStudent = GetStudent();
+            Print(firstStudent);
+        }
+
+        static Student GetStudent()
+        {
+            Student student = new Student();
+            student.id = Guid.NewGuid();
+            student.name = "Плеханов Андрей Николаевич";
+            student.age = 17;
+            student.group = "НТ-081";
+
+            return student;
+        }
+
+        static void Print(Student student)
+        {
+            Console.WriteLine("id: " + student.id);
+            Console.WriteLine("ФИО: " + student.name);
+            Console.WriteLine("Возраст: " + student.age);
+            Console.WriteLine("Группа: " + student.group);
+        }
+    }
+}
---
+++ Learning CH/ClassLearning/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLearning
+{
+    class Student
+    {
+        public Guid id;
+        public string name;
+        public int age;
+        public string group;
+    }
+}
