2a6847af3c5262f9d1105fa0eb96b7570617aaf5
Andrey Plekhanov <ierehon91@gmail.com>
2022-02-17 10:27:03 +0300

    Добавлен проект get set propertys

---
+++ Learning CH/GetSetPropertys/Point.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetSetPropertys
+{
+    class Point
+    {
+        private int x;
+        private int y;
+        private int z;
+
+        public int X
+        {
+            get 
+            {
+                return x;
+            }
+
+            set 
+            { 
+                if (value < 0)
+                {
+                    x = 0;
+                    return;
+                }
+                if (value > 100)
+                {
+                    x = 100;
+                    return;
+                }
+                x = value; 
+            }
+        }
+        
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    y = 0;
+                    return;
+                }
+                if (value > 100)
+                {
+                    y = 100;
+                    return;
+                }
+                y = value;
+            }
+        }
+
+        public int Z { get; set; } // автомотическое свойство (в VS набираем prop)
+
+    }
+}
---
+++ Learning CH/GetSetPropertys/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GetSetPropertys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+            point.X = 10;
+            point.Y = 200;
+            point.Z = 825;
+
+            Console.WriteLine("X: {0}\nY: {1}\nZ: {2}", point.X, point.Y, point.Z);
+        }
+    }
+}
