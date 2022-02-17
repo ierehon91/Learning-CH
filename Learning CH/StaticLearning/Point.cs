f0e4fbb2fd2ed8929813e5eb9a77731752bfb5b1
Andrey Plekhanov <ierehon91@gmail.com>
2022-02-17 14:32:06 +0300

    Создан проект Статических полей, метадов, свойств, конструкоторв

---
+++ Learning CH/StaticLearning/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticLearning
+{
+    class Point
+    {
+        private static int x;
+        private static int y;
+
+        public void SetX(int x) { Point.x = x; }
+        public void GetX() { Console.WriteLine(Point.x); }
+        public void SetY(int y) { Point.y = y; }
+        public void GetY() { Console.WriteLine(Point.y); }
+
+    }
+}
---
+++ Learning CH/StaticLearning/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point();
+            Point point2 = new Point();
+
+            point1.SetX(10);  // записываем значение 10 в статическое поле x класса Point
+            point1.SetY(-15); // записываем значение 10 в статическое поле y класса Point
+
+            point2.GetX();  // 10, так как статические поля для каждого экземпляра объекта общие
+            point2.GetY();  // -15
+        }
+    }
+}
