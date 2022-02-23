e8db22b31d72e17b32ce8257fec16bc2c76d2b8d
Andrey Plekhanov <ierehon91@gmail.com>
2022-02-23 20:24:49 +0300

    Добавлен проект с примером работы модицикатора доступа protected

---
+++ Learning CH/Inheritance_protected_mod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Inheritance_protected_mod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            Console.WriteLine(a.publicField);
+            // Console.WriteLine(a.privateField); // ошибка доступа
+            // Console.WriteLine(a.protectedField); // ошибка доступа
+        }
+    }
+
+    class A
+    {
+        public int publicField = 1;
+        private int privateField = 2;
+        protected int protectedField = 3;
+    }
+
+    class B : A
+    {
+        public void PrintB()
+        {
+            Console.WriteLine(publicField);
+            // Console.WriteLine(privateField); // ошибка доступа
+            Console.WriteLine(protectedField);
+        }
+    }
+}
