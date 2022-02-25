b66f85e807b54c8b66a89a4b205cae290b71c969
Andrey Plekhanov <ierehon91@gmail.com>
2022-02-25 08:44:15 +0300

    Создан проект изучения темы полиморфизма. Разобрана тема виртуальных методов, применение ключевых слов virtual и override.

---
+++ Learning CH/Polymorphism/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Polymorphism
+{
+
+    class Person
+    {
+        public void Drive(Car car)
+        {
+            car.Drive();
+        }
+    }
+
+    class Car
+    {
+        protected void StartEngine()
+        {
+            Console.WriteLine("Запуск двигателя!");
+        }
+
+        public virtual void Drive()
+        {
+            StartEngine();
+            Console.WriteLine("Машина поехала!");
+        }
+    }
+
+    class SportCar : Car
+    {
+        public override void Drive()
+        {
+            base.Drive();
+            Console.WriteLine("Очень очень быстро, ведь это спорткар!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Car car = new Car();
+            Car sportCar = new SportCar();
+
+            person.Drive(car);
+            Console.WriteLine("\n");
+            person.Drive(sportCar);
+        }
+    }
+}
