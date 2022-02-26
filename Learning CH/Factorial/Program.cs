39574073fa48c695f83735f10b2a98cab40c490f
Andrey Plekhanov <ierehon91@gmail.com>
2022-02-26 10:45:15 +0300

    Добавлен проект написания факториала

---
+++ Learning CH/Factorial/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunFactorial(15);
+        }
+
+        static int Factorial( int num )
+        {
+            if (num < 1)
+            {
+                return 0;
+            }
+            
+            else if (num == 1)
+            {
+                return 1;
+            }
+
+            else
+            {
+                checked
+                {
+                    return num * Factorial(num - 1);
+                }
+            }
+        }
+
+        static void RunFactorial( int num )
+        {
+            if ( num <= 15)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    try
+                    {
+                        Console.WriteLine($"{i}! = {Factorial(i)}");
+                    }
+                    catch (System.OverflowException)
+                    {
+                        Console.WriteLine($"{i}! = Ошибка переполнения!");
+                    }
+                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("Максимальное число для которого выполняется подсчёт факториала = 15");
+            }
+        }
+    }
+} 
